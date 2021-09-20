c3783e1fd0bc9dde0460b822a142f2606038a25c
blvkparis <popkov99@yandex.ru>
2021-09-20 21:10:06 +0300

    Added svg to Favourite button (DONE!)

--- src/main/resources/static/js/favourite.js
+++ src/main/resources/static/js/favourite.js
@@ -12,12 +12,18 @@ function changeAction() {
 
 function drawButton() {
     let btn = document.getElementById("favouriteButton");
-    let url="http://localhost:8080/ads/"+id+"/getButton";
-    $.get(url, function (response){
-        if(response==true){
-            btn.innerHTML="Убрать из избранного";
+    let url = "http://localhost:8080/ads/" + id + "/getButton";
+    let favIcon = document.getElementById("favourite-icon");
+    $.get(url, function (response) {
+        if (response == true) {
+            btn.style["filter"] = "invert(8%) sepia(100%) saturate(7195%) hue-rotate(14deg) brightness(102%) contrast(105%)";
+            btn.title = "Убрать из избранного";
+            favIcon.src = "/images/heart-solid.svg";
+        } else {
+            btn.style["filter"] = "none";
+            btn.title = "Добавить в избранное";
+            favIcon.src = "/images/heart-regular.svg";
         }
-        else btn.innerHTML="Добавить в избранное";
     });
 }
 
