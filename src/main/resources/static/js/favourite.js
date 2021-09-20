09d7527b59129ec8f5ad2c3dbffcfeacf7627837
Kaaterinka <katerinabolotina@inbox.ru>
2021-09-20 17:11:11 +0300

    Actions for favourite ads

---
+++ src/main/resources/static/js/favourite.js
@@ -0,0 +1,22 @@
+function changeAction() {
+    let btn = document.getElementById("favouriteButton");
+    $.ajax({
+        url: "http://localhost:8080/ads/" + id + "/favourite",
+        type: "post",
+        cache: false
+    }).done(function (data) {
+            drawButton();
+        }
+    );
+}
+
+function drawButton() {
+    let btn = document.getElementById("favouriteButton");
+    let url="http://localhost:8080/ads/"+id+"/getButton";
+    $.get(url, function (response){
+        if(response==true){
+            btn.innerHTML="Убрать из избранного";
+        }
+        else btn.innerHTML="Добавить в избранное";
+    });
+}
